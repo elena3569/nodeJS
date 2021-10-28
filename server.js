7bf2f3515efceabd6d8dbb557590a182beca9281
Elena <elenashevtsova380@gmail.com>
2021-10-28 10:31:58 +0300

    les6

--- server.js
+++ server.js
@@ -1,83 +1,60 @@
-const fs = require('fs')
-const path = require('path');
-const inquirer = require('inquirer');
-const readline = require('readline')
-const cluster = require('cluster')
+const socket = require('socket.io')
 const http = require('http')
-const os = require('os')
-const url = require('url')
+const path = require('path')
+const fs = require('fs')
 
-
-const isFile = fileName => {
-    return fs.lstatSync(fileName).isFile();
-}
-
-let currentDirectory = 'D:/'
-
-const port = 5555
-
-if (cluster.isMaster) {
-    
-    for (let i = 0; i < os.cpus().length; i++) {
-      cluster.fork();
-    }
-  } else {
-
-    http.createServer((request, response) => {
-    
-    let navigationBlock = `<div style="border: 1px solid black; padding: 10px">`
-    let fileContent = `<div style="border: 1px solid black; padding: 10px; width: 500px">`
-
-        if (request.method === 'GET') {
-            const queryParams = url.parse(request.url, true).query;
-            if (request.url === "/") {
-                currentDirectory = 'D:/'
-            } else {
-                if (request.url != '/favicon.ico') {   
-                    if (isFile(path.resolve(request.url.substr(1)))) {
-                        const filePath = path.resolve(request.url.substr(1));
-                        const data = fs.readFileSync(filePath)
-                        fileContent += `${data.toString('utf8')}`;
-                        currentDirectory = path.dirname(path.resolve(request.url.substr(1)))
-
-                    } else {
-                        currentDirectory = path.resolve(request.url.substr(1))
-                    }
-                    
-                }
-            }
-            
-            let pathUrl = path.resolve(currentDirectory, '../').replace(/(\\|\/)/g, '/') ;
-            listDirectory = `<ul class="directoriesList"> <li><a href="http://localhost:${port}/${pathUrl}">../</a></li>`
-            for (let i = 0; i < fs.readdirSync(currentDirectory).length; i++) {
-                pathUrl = currentDirectory.replace(/(\\|\/)/g, '/') + '/' + fs.readdirSync(currentDirectory)[i];
-                listDirectory += `<li><a href="http://localhost:${port}/${pathUrl}">${fs.readdirSync(currentDirectory)[i]}</a></li>`
-            }
-        listDirectory += '</ul>'
-        navigationBlock += listDirectory + '</div>'
-        fileContent += '</div>'
-        const documentContent = `<div style="display: flex">` + navigationBlock + fileContent + '</div>'
-
-        response.writeHead(200, { 'Content-Type': 'text/html'});
-        response.end(documentContent);
-
-        } else if (request.method === 'POST') {
-        let data = '';
-
-        request.on('data', chunk => {
-            data += chunk;
-        });
-
-        request.on('end', () => {
-            const parsedData = JSON.parse(data);
-            console.log(parsedData);
-
-            response.writeHead(200, { 'Content-Type': 'json'});
-            response.end(data);
-        });
-        } else {
+const app = http.createServer((request, response) => {
+    if (request.method === 'GET') {
+          
+      const filePath = path.join(__dirname, 'index.html');
+  
+      readStream = fs.createReadStream(filePath);
+  
+      readStream.pipe(response);
+    } else if (request.method === 'POST') {
+      let data = '';
+  
+      request.on('data', chunk => {
+      data += chunk;
+      });
+  
+      request.on('end', () => {
+        const parsedData = JSON.parse(data);
+        console.log(parsedData);
+  
+        response.writeHead(200, { 'Content-Type': 'json'});
+        response.end(data);
+      });
+    } else {
         response.statusCode = 405;
         response.end();
-        }  
-    }).listen(port, 'localhost');
-  }
+    }
+  });
+
+const io = socket(app);
+
+let countUsers = 0
+
+io.on('connection', client => {
+  countUsers++
+  const id = 'user' + Math.floor(Math.random()*100)
+  
+  client.broadcast.emit('SERVER_MSG', { msg: `The ${id} connected`, userCount: countUsers  });
+  client.emit('SERVER_MSG', { msg: `The ${id} connected`, userCount: countUsers  });
+
+  client.on('CLIENT_MSG', (data) => {
+    client.broadcast.emit('SERVER_MSG', { msg: id + ': ' + data.msg, userCount: countUsers  });
+    client.emit('SERVER_MSG', { msg: id + ': ' + data.msg, userCount: countUsers });
+  });
+
+  
+  client.on('disconnect', () => {
+    countUsers--
+    client.broadcast.emit('SERVER_MSG', { msg: id + 'disconnected', userCount: countUsers});
+    client.emit('SERVER_MSG', { msg: id + 'disconnected', userCount: countUsers});
+    client.emit('CHANGED_NUM_USERS', {msg: countUsers})
+  })
+});
+
+
+app.listen(3001, 'localhost');
